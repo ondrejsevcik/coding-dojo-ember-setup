3c59f513dae7916e0b61d385db46a5b80550afe6
Ondrej Sevcik <ondrej.sev@gmail.com>
2020-06-30 08:17:36 +0200

    Init

---
+++ tests/integration/components/empty-test.js
@@ -0,0 +1,10 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | empty-test', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it works', async function(assert) {
+    assert.equal(true, true);
+  });
+});
